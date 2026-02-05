b8b9b22d1be281e05661720ab2236ffc0300ee27
Simone Ceriani <sceriani@nvidia.com>
2026-02-05 23:56:05 +0100

    fix bat with call

--- build-repository-windows.bat
+++ build-repository-windows.bat
@@ -1,9 +1,11 @@
+echo on
+
 mkdir ..\build\windows-msvc2022-x64
 mkdir ..\install\windows-msvc2022-x64
 
 cd ..\build\windows-msvc2022-x64
 
-..\..\qt6\configure.bat -prefix ..\..\install\windows-msvc2022-x64
+call ..\..\qt6\configure.bat -prefix ..\..\install\windows-msvc2022-x64
 
 cmake --build .
 cmake --install .
