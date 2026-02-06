1c534e0d061fb5f36b5a8d6b606b99243e320dee
Simone Ceriani <sceriani@nvidia.com>
2026-02-06 16:09:17 +0100

    fix win again

--- build-repository-windows.bat
+++ build-repository-windows.bat
@@ -5,7 +5,7 @@ mkdir ..\install\windows-msvc2022-x64\qt6
 
 cd ..\build\windows-msvc2022-x64\qt6
 
-call ..\..\..\qt6\configure.bat -prefix ..\..\install\windows-msvc2022-x64\qt6\
+call ..\..\..\qt6\configure.bat -prefix ..\..\..\install\windows-msvc2022-x64\qt6\
 
 cmake --build .
 cmake --install .
