d11dbe7e9da01f72e2327bf7c0ca346a00d7441a
Simone Ceriani <sceriani@nvidia.com>
2026-02-18 15:09:24 +0100

    fix linux install path

--- build-repository-linux.sh
+++ build-repository-linux.sh
@@ -5,7 +5,7 @@ mkdir -p ../install/linux-gcc/qt6/
 
 cd ../build/linux-gcc/qt6/
 
-../../../qt6/configure -prefix ../../install/linux-gcc/qt6/
+../../../qt6/configure -prefix ../../../install/linux-gcc/qt6/
 
 cmake --build . --parallel 4
 cmake --install .
